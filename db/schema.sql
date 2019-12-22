3832c3bdcf36cc872ef75dcf2551e35146e1cbb7
andyrich75 <arichard101@gmail.com>
2019-12-22 09:56:39 -0500

    added the table for role

--- db/schema.sql
+++ db/schema.sql
@@ -9,4 +9,13 @@ name varchar(30),
 PRIMARY KEY(id)
 );
 
+create table roles(
+id int auto_increment,
+title varchar(30),
+salary decimal,
+department_id int,
+PRIMARY KEY (id),
+FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+
 
